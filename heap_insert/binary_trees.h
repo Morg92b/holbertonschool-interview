8ac048aa3ca6e0dcc5d4f80571262b12924b742c
Morg92b <morgan.bouaziz@outlook.fr>
2026-04-20 11:38:33 +0200

    fix betty

--- heap_insert/binary_trees.h
+++ heap_insert/binary_trees.h
@@ -13,16 +13,16 @@
  */
 typedef struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 } binary_tree_t;
 
 typedef struct binary_tree_s heap_t;
 
 void binary_tree_print(const binary_tree_t *);
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value); /* <-- function importée */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 heap_t *heap_insert(heap_t **root, int value);
 heap_t *createNode(int value);
 int count_nodes(heap_t *node);
